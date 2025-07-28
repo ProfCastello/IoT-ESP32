7e37ebe3ea9af960a5ef316f5fa1c1cfc625f583
ProfCastello <gustavo.castello@sp.senai.br>
2025-07-28 16:05:30 -0300

    refactor: Ajustar estrutura do CSS e HTML, melhorar a conexão MQTT e processar mensagens recebidas

--- 01-introducaoWeb/js/script.js
+++ 01-introducaoWeb/js/script.js
@@ -1,19 +1,56 @@
-const temperaturas = [
-  { id: 1, temperatura: 23.9 },
-  { id: 2, temperatura: 25.4 },
-  { id: 3, temperatura: 21.1 },
-  { id: 4, temperatura: 32.7 },
-];
+let clienteWeb = null;
 
-function simularLeitura() {
-  //   alert("Temperatura: 23.5 °C");
-  //   Math.floor faz o arredondamento
-  //   Math.random gera um número aleatório menor que 1
+const clientId = "Esp32" + Math.floor(Math.random() * 900) + 100;
+clienteWeb = new Paho.MQTT.Client("broker.hivemq.com", 8884, clientId);
 
-  const numeroSorteado = Math.floor(Math.random() * temperaturas.length);
+// Obtem acesso ao elemento do HTML
+const tempPagina = document.getElementById("temperatura");
+const umidadePagina = document.getElementById("umidade");
+const timePagina = document.getElementById("time");
 
-  const tempSorteada = temperaturas[numeroSorteado].temperatura;
+// Define o callback para quando a mensagem chegar
+clienteWeb.onMessageArrived = onMessage;
 
-  const temperaturaPagina = document.getElementById("temperatura");
-  temperaturaPagina.textContent = `${tempSorteada} °C `;
+// Conecta ao Broker MQTT
+function connectBroker() {
+  clienteWeb.connect({
+    useSSL: true,
+    onSuccess: function () {
+      alert("Conexão com Broker bem sucedida");
+      clienteWeb.subscribe("senai510/temperatura/m");
+    },
+    onFailure: function () {
+      alert("Falha ao conectar ao Broker");
+    },
+  });
 }
+
+// Define o callback para quando a conexão for perdida
+clienteWeb.onConnectionLost = function (responseObject) {
+  if (responseObject.errorCode !== 0) {
+    alert("A conexão com o Broker foi perdida: " + responseObject.errorMessage);
+  }
+  console.log("Conexão perdida: " + responseObject.errorMessage);
+  tempPagina.textContent = "Conexão perdida";
+  umidadePagina.textContent = "";
+  timePagina.textContent = "";
+
+  // Tenta reconectar após 5 segundos
+  setTimeout(connectBroker, 5000);
+};
+
+// Define a função para processar mensagens recebidas
+function onMessage(message) {
+  const payload = message.payloadString;
+  const dados = JSON.parse(payload);
+
+  tempPagina.textContent = String(dados.temperatura) + " °C";
+  umidadePagina.textContent = String(dados.umidade) + " %";
+
+  const agora = new Date();
+  const dataPtBR = agora.toLocaleString();
+  timePagina.textContent = "Atualizado em: " + dataPtBR;
+}
+
+// Inicia a conexão
+connectBroker();
